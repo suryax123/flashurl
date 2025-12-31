e944ab0e7f436523ab7cbe4bbe489106b0ca6cb6
suryax123 <diyhacker71@gmail.com>
2025-12-31 08:31:03 +0530

    Fix pre-save hooks and remove stray marker in user model

--- models/user.js
+++ models/user.js
@@ -144,19 +144,14 @@ const userSchema = new mongoose.Schema({
     }
 });
 
-// Hash password before saving
-userSchema.pre('save', async function(next) {
+// Hash password before saving (async hook without next)
+userSchema.pre('save', async function() {
     if (!this.isModified('password') || !this.password) {
-        return next();
+        return;
     }
     
-    try {
-        const salt = await bcrypt.genSalt(10);
-        this.password = await bcrypt.hash(this.password, salt);
-        next();
-    } catch (error) {
-        next(error);
-    }
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
 });
 
 // Generate referral code before saving
